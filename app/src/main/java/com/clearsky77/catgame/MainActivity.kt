73ccc877797f86d655fabb112fdc70a06434a3ce
clearsky77 <clearsky3264@gmail.com>
2022-01-13 23:58:47 +0900

    바인딩으로 변경 / 광고 로드까지 완료(아직 표시X)

--- app/src/main/java/com/clearsky77/catgame/MainActivity.kt
+++ app/src/main/java/com/clearsky77/catgame/MainActivity.kt
@@ -1,17 +1,18 @@
 package com.clearsky77.catgame
 import android.os.Bundle
 import android.util.DisplayMetrics
+import android.util.Log
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import com.google.android.gms.ads.AdRequest
-import com.google.android.gms.ads.AdSize
-import com.google.android.gms.ads.AdView
-import com.google.android.gms.ads.MobileAds
+import com.clearsky77.catgame.databinding.ActivityMainBinding
+import com.google.android.gms.ads.*
 import com.google.android.gms.ads.rewarded.RewardedAd
-import kotlinx.android.synthetic.main.activity_main.*
+import com.google.android.gms.ads.rewarded.RewardedAdLoadCallback
 
 /** Main Activity. Inflates main activity xml and child fragments.  */
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityMainBinding
     private lateinit var adView: AdView
 
     private var mRewardedAd: RewardedAd? = null
@@ -23,6 +24,19 @@ class MainActivity : AppCompatActivity() {
         private val AD_REWARD_ID = "ca-app-pub-3940256099942544/5224354917" // 보상형 아이디(구글 테스트용 ID)
     }
 
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setValues()
+        setupEvents()
+    }
+
     private val adSize: AdSize // 화면 가로 사이즈
         get() {
             val display = windowManager.defaultDisplay
@@ -31,7 +45,7 @@ class MainActivity : AppCompatActivity() {
 
             val density = outMetrics.density
 
-            var adWidthPixels = ad_view_container.width.toFloat()
+            var adWidthPixels = binding.adViewContainer.width.toFloat()
             if (adWidthPixels == 0f) {
                 adWidthPixels = outMetrics.widthPixels.toFloat()
             }
@@ -40,23 +54,20 @@ class MainActivity : AppCompatActivity() {
             return AdSize.getCurrentOrientationAnchoredAdaptiveBannerAdSize(this, adWidth)
         }
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
-        setValues()
-        setupEvents()
-    }
-
     private fun setValues() {
+
         MobileAds.initialize(this) { } // 애드몹 초기화
         adView = AdView(this)
-        ad_view_container.addView(adView) // 레이아웃에 에드뷰 연결
+        binding.adViewContainer.addView(adView) // 레이아웃에 에드뷰 연결
         loadBanner()
     }
 
     private fun setupEvents(){
+        binding.btnGetReward.setOnClickListener {
+            Log.d(TAG, "클릭 리스너 진입")
+            loadInterstitialAd() // 광고 로드
 
+        }
     }
 
 
@@ -67,6 +78,22 @@ class MainActivity : AppCompatActivity() {
         val adRequest = AdRequest.Builder().build()
 
         adView.loadAd(adRequest) // 애드뷰 로드
+    }
+
+    private fun loadInterstitialAd() {
+        var adRequest = AdRequest.Builder().build()
+
+        RewardedAd.load(this,AD_REWARD_ID, adRequest, object : RewardedAdLoadCallback() {
+            override fun onAdFailedToLoad(adError: LoadAdError) {
+                Log.d(TAG, adError?.message)
+                mRewardedAd = null
+            }
+
+            override fun onAdLoaded(rewardedAd: RewardedAd) {
+                Log.d(TAG, "Ad was loaded.")
+                mRewardedAd = rewardedAd
+            }
+        })
     }
 
 
